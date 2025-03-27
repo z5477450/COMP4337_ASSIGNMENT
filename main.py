725f30763774c921f2775981846b98aca6fcc916
David <z5477450@ad.unsw.edu.au>
2025-03-28 09:25:56 +1100

    Finished generating pieces of the secret key using shamir secret sharing technique

--- main.py
+++ main.py
@@ -3,18 +3,52 @@ import time
 import threading
 from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from cryptography.hazmat.primitives import serialization
+import random
+
+def ShamirGraphicalFunction(x, coefficients, secret):
+    y = secret
+    degree = len(coefficients) + 1
+    for value in coefficients:
+        y += value * (x)^degree
+        degree -= 1
+    return y
+
+def shamirSecretSharing(secret):
+    k = int(sys.argv[2])
+    n = int(sys.argv[3])
+
+    degree = k -1 
+    pieces = []
+    # These are randomly generated coefficients for the shamir f(x) function.
+    coefficients = []
+
+    for i in range(0, degree):
+        coefficients.append(random.randint(1, 100))
+
+
+    for i in range(1, n+1):
+        share = ShamirGraphicalFunction(i, coefficients, secret)
+        pieces.append((i, share))
+
+        
+
+    
+
 
 
 def generateEphemeral(t):
     while True:
-        EphID_gen = X25519PrivateKey.generate()
-        EphID = EphID_gen.public_key()
-        
+        EphID_priv = X25519PrivateKey.generate()
+        EphID = EphID_priv.public_key()
+
         EphID_bytes = EphID.public_bytes(
             encoding=serialization.Encoding.Raw, 
             format=serialization.PublicFormat.Raw 
         )
         EphID_int = int.from_bytes(EphID_bytes, byteorder='big')
+
+        shamirSecretSharing(EphID_int)
+
         time.sleep(t)
 
 def main():
@@ -30,15 +64,19 @@ def main():
 
     if t not in tSet:
         print(f"t value must be an element of the set {tSet}")
+        print("Usage: python3 Dimpy.py [t] [k] [n]")
         sys.exit(1)
     elif k < 3:
         print("k must be greater than or equal to 3")
+        print("Usage: python3 Dimpy.py [t] [k] [n]")
         sys.exit(1)
     elif n < 5: 
         print("n must be greater than or equal to 5")
+        print("Usage: python3 Dimpy.py [t] [k] [n]")
         sys.exit(1)
     elif k >= n:
         print("k must be less than n")
+        print("Usage: python3 Dimpy.py [t] [k] [n]")
         sys.exit(1)
 
     ephemeralIDThread = threading.Thread(target=generateEphemeral, args=(t,), daemon=True)  
@@ -48,10 +86,9 @@ def main():
     try:
         while True:
             k -= 1
-            
-
     except Exception as e:
         print("Closing program.")
+
         sys.exit(1)
 
 
